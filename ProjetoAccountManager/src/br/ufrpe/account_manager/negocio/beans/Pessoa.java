88bc8964930e729fe25d2777e5f8547e2ff2a7dc
jhon sidney <jhon.sidney@outlook.com>
2017-11-13 18:57:45 -0200

    alteração em Pessoa

--- ProjetoAccountManager/src/br/ufrpe/account_manager/negocio/beans/Pessoa.java
+++ ProjetoAccountManager/src/br/ufrpe/account_manager/negocio/beans/Pessoa.java
@@ -1,5 +1,84 @@
 package br.ufrpe.account_manager.negocio.beans;
 
-public class Pessoa {
+import java.util.Date;
+
+public class  Pessoa {
+
+	String nome;
+	String cpf;
+	String email;
+	String senha;
+	String logradouro;
+	Date nascimento;
+	String tel;
+
+	public Pessoa(String nome, String cpf, String email, String senha, String logradouro, Date nascimento, String tel) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+		this.logradouro = logradouro;
+		this.nascimento = nascimento;
+		this.tel = tel;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public Date getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(Date nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
 
 }
