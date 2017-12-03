6d0d7f7470e7697f1c6107379d796229c6e2ca6a
jhon sidney <jhon.sidney@outlook.com>
2017-12-03 17:34:52 -0200

    teste1 em dev

---
+++ ProjetoAccountManager/src/br/ufrpe/account_manager/ClasseDeTeste.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package br.ufrpe.account_manager;
+
+/**
+ * @author sidne
+ *
+ */
+public class ClasseDeTeste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world teste");
+		
+	}
+
+}
