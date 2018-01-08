2a9e742a8b3976fa8a3cd7d26a3614f48443b1cf
Felipe9 <felipe.cavalcanty@hotmail.com>
2018-01-08 17:49:55 -0300

    Editando Felipe comitou: Começo do MeuPerfil

--- ProjetoAccountManager/src/br/ufrpe/account_manager/gui/TelaMenuPerfilController.java
+++ ProjetoAccountManager/src/br/ufrpe/account_manager/gui/TelaMenuPerfilController.java
@@ -1,5 +1,41 @@
 package br.ufrpe.account_manager.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class TelaMenuPerfilController {
+	
+@FXML
+private TextField TelaMenuPerfil_TF_Nome;
+
+@FXML
+private TextField TelaMenuPerfil_TF_Sobrenome;
+
+@FXML
+private TextField TelaMenuPerfil_TF_Cpf;
+
+@FXML
+private TextField TelaMenuPerfil_TF_Salario;
+
+@FXML
+private TextField TelaMenuPerfil_TF_Id;
+
+@FXML
+private TextField TelaMenuPerfil_TF_Email;
+
+@FXML
+private PasswordField TelaMenuPerfil_TF_Senha;
+
+@FXML
+private TextField TelaMenuPerfil_TF_Logradouro;
+
+@FXML
+private TextField TelaMenuPerfil_TF_Nascimento;
+
+@FXML
+private TextField TelaMenuPerfil_TF_Telefone;
+
+
 
 }
