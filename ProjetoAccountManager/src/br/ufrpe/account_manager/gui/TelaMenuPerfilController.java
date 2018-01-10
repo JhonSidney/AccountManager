baf27832133265bda6ace1aee8b735e3deb0ed7d
Felipe9 <felipe.cavalcanty@hotmail.com>
2018-01-10 15:26:55 -0300

    Editando Felipe comitou: Estruturação da classe TelaMenuPerfil

--- ProjetoAccountManager/src/br/ufrpe/account_manager/gui/TelaMenuPerfilController.java
+++ ProjetoAccountManager/src/br/ufrpe/account_manager/gui/TelaMenuPerfilController.java
@@ -1,9 +1,19 @@
 package br.ufrpe.account_manager.gui;
 
+import java.io.IOException;
+
 import br.ufrpe.account_manager.SistemaAccountManager;
+import br.ufrpe.account_manager.negocio.beans.Pessoa;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 
 public class TelaMenuPerfilController {
 
@@ -39,4 +49,23 @@ public class TelaMenuPerfilController {
 
 	String nome, sobrenome, cpf, salario, id, email, senha, logradouro, nascimento, telefone;
 	SistemaAccountManager fachada;
+	
+	public void initialize() {
+		SistemaAccountManager.getInstance();
+	}
+	
+	@FXML
+	public void botao_atualizar(ActionEvent event) throws IOException {
+		
+	}
+	
+	@FXML
+	public void botao_voltar(ActionEvent event) throws IOException {
+		
+	}
+	
+	@FXML
+	public void botao_excluirMeuPerfil(ActionEvent event) throws IOException {
+		
+	}
 }
