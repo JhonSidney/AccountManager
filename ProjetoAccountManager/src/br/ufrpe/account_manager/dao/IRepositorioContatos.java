a6b6113d51276aedc702e9f20c42718370ea0faa
lucas sidronio <lucassidroniox@gmail.com>
2018-01-16 13:11:04 -0300

    Editando Sidronio commitou: Alteração em controlador de contatos.

--- ProjetoAccountManager/src/br/ufrpe/account_manager/dao/IRepositorioContatos.java
+++ ProjetoAccountManager/src/br/ufrpe/account_manager/dao/IRepositorioContatos.java
@@ -7,13 +7,6 @@ import br.ufrpe.account_manager.negocio.beans.Pessoa;
 
 public interface IRepositorioContatos {
 
-	/*public void cadastrar(Contato contato);
-	public void remover(Contato contato);
-	public void atualizar (Contato contato);
-	public boolean existe(Contato contato);
-	public ArrayList<Contato> listar();
-	public Contato procurar (Contato contato);
-	*/
 	
 	public boolean cadastrar(Contato contato);
 	public boolean atualizar(Contato contato);
---
+++ ProjetoAccountManager/src/br/ufrpe/account_manager/negocio/ControladorContato.java
@@ -0,0 +1,76 @@
+package br.ufrpe.account_manager.negocio;
+
+import java.util.ArrayList;
+
+import br.ufrpe.account_manager.dao.RepositorioContatos;
+import br.ufrpe.account_manager.dao.RepositorioPessoas;
+import br.ufrpe.account_manager.exception.NegocioException;
+import br.ufrpe.account_manager.negocio.beans.Contato;
+import br.ufrpe.account_manager.negocio.beans.Pessoa;
+
+public class ControladorContato{
+
+	private RepositorioContatos repositorioContato;
+	private static ControladorContato instance;
+
+	public ControladorContato() {
+		this.repositorioContato = RepositorioContatos.getInstance();
+	}
+
+	public static ControladorContato getInstance() {
+		if (instance == null) {
+			instance = new ControladorContato();
+		}
+		return instance;
+	}
+
+	public void cadastrar(Contato contato) throws NegocioException {
+		if (contato != null && !this.existe(contato.getCpf())) {
+			this.repositorioContato.cadastrar(contato);
+		} else {
+			throw new NegocioException("Usuario já Existe !");
+		}
+	}
+
+	public void atualizar(Contato contato) throws NegocioException {
+		if (contato != null && this.existe(contato.getCpf())) {
+			this.repositorioContato.atualizar(contato);
+
+		} else {
+			throw new NegocioException("Usuario Não Existe !");
+		}
+	}
+
+	public Contato procurar(String cpf) throws NegocioException {
+		Contato resultado = this.repositorioContato.procurar(cpf);
+		return resultado;
+	}
+	
+	
+
+	public ArrayList<Contato> listar() {
+		return this.repositorioContato.listar();
+	}
+
+	private boolean existe(String cpf) {
+		ArrayList<Contato> retorno = this.repositorioContato.listar();
+		for (Contato contato : retorno) {
+			if (contato.getCpf().equals(cpf)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void remover(String cpf) throws NegocioException {
+		Contato contato = this.repositorioContato.procurar(cpf);
+
+		if (contato != null) {
+			this.repositorioContato.remover(cpf);
+		} else {
+			throw new NegocioException("Usuário Não Existe !");
+		}
+	}
+
+
+}
